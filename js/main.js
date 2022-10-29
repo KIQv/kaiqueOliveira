52eeb31a2ac7c0a40740b0f88970af6fb89c2332
KIQ <kaiqueoliveira257@gmail.com>
2022-10-28 22:53:14 -0700

    Menu responsivo finalizado
    Banner home finalizado na resolução 1920x
    
    Biblioteca Typed adicionada

--- js/main.js
+++ js/main.js
@@ -3,3 +3,27 @@ var cursor2 = document.querySelector(".cursor2");
 document.addEventListener("mousemove",function(e){
     cursor.style.cssText = cursor2.style.cssText = "left: " + e.clientX + "px; top: " + e.clientY + "px;";
 });
+
+const menuHamburguer = document.querySelector(".menuHamburguer");
+const navMenu = document.querySelector(".navMenu");
+const navIdioma = document.querySelector(".navIdioma");
+
+menuHamburguer.addEventListener("click", () => {
+    menuHamburguer.classList.toggle('ativo');
+    navMenu.classList.toggle('ativo');
+    navIdioma.classList.toggle('ativo');
+});
+
+var typed = new Typed(".typed",{
+    strings : [
+        'senvolvedor',
+        'signer'
+      ],
+    smartBackspace: false,
+    typeSpeed : 100,
+    backSpeed : 80,
+    startDelay : 1000,
+    backDelay : 1500,
+    loop : true,
+    loopCount : Infinity,
+});
